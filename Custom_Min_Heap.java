740bb08f88c0c39000cc7f320f66406c887bd242
jiamin1013 <35900323+jiamin1013@users.noreply.github.com>
2018-02-24 18:21:43 -0500

    Add files via upload

--- Custom_Min_Heap.java
+++ Custom_Min_Heap.java
@@ -1,4 +1,4 @@
-import java.util.NoSuchElementException;
+
 
 //Custom min heap using array
 
@@ -7,11 +7,13 @@ public class Custom_Min_Heap
 	protected item heap[];
 	protected int capacity;
 	protected int currentIndex;
+	protected int [] bookeep;
 
 	public Custom_Min_Heap(int size) {
 		this.capacity = size;
 		this.currentIndex = 0;
 		this.heap = new item [size];
+		this.bookeep = new int [size+1]; //+1 to skip index zero value
 	}
 	
 	public boolean isEmpty() {
@@ -25,6 +27,7 @@ public class Custom_Min_Heap
 	public void add(item vertex) {
 		if (!isFull()) {
 			heap[currentIndex] = vertex;
+			bookeep[vertex.V]  = currentIndex;
 			heapifyUp(currentIndex,vertex.key);
 			currentIndex++;
 		}else throw new IndexOutOfBoundsException("OVERFLOW");	
@@ -32,17 +35,29 @@ public class Custom_Min_Heap
 	
 	public item extract_min () {
 		if(!isEmpty()) {
-			return heapifyDown();
+			item min = heap[0];	//get min
+			int weight = heap[currentIndex-1].key; //get last node key
+			//switch root and last node
+			swap(0,currentIndex-1);
+			//assign last node (min) to null
+			heap[currentIndex-1] = null;
+			currentIndex--;
+			heapifyDown(0, weight);
+			return min;
 		}else throw new IndexOutOfBoundsException("OVERFLOW");		
 	}
 	
 	
 	protected void swap(int pos1, int pos2) {
+		//swap index info first
+		bookeep[heap[pos1].V] = pos2;
+		bookeep[heap[pos2].V] = pos1;
+		//swap content next
 		item temp = heap[pos1];
 		heap[pos1] = heap[pos2];
 		heap[pos2] = temp;
 	}
-	
+
 	public void heapifyUp (int index, int weight) {
 		if(hasParent(index)) {
 			int parentIndex = getParentIndex(index);
@@ -56,28 +71,18 @@ public class Custom_Min_Heap
 		}
 	}
 	
-	public item heapifyDown () {
-		int index = 0;
+	public void heapifyDown (int index, int weight) {
 		int left;
 		int right;
-		item min = heap[0];	//get min
-		int weight = heap[currentIndex-1].key; //get last node key
-		
-		//switch root and last node
-		swap(0,currentIndex-1);
-		//assign last node (min) to null
-		heap[currentIndex-1] = null;
-		currentIndex--;
-		
 		while (index < currentIndex) {
-			 if (!(hasLeftChild(index)) && !(hasRightChild(index))) {return min;}
+			 if (!(hasLeftChild(index)) && !(hasRightChild(index))) {return;}
 			 else if(hasLeftChild(index) && !(hasRightChild(index))) {
 				//only has a left child -- leaf condition
 				left = getLeftChildIndex(index);
 				if(heap[index].key > heap[left].key) {
 					swap(left,index);
 					index = left;
-				}else return min;
+				}else return ;
 				
 			}else{		//not a leaf condition
 				left = getLeftChildIndex(index);
@@ -88,15 +93,26 @@ public class Custom_Min_Heap
 				}else if (heap[right].key > heap[left].key && weight > heap[left].key) {
 					swap(left,index);
 					index = left;
-				}else return min;
+				}else return ;
 			}
 		}
-		return min;
+		return ;
 	}
 	
-	public int getParentIndex(int childIndex) {
-		return (childIndex+1)/2-1;
+	public void decreaseKey(int i, int weight) {
+	    if (heap[bookeep[i]].key < weight) {
+
+	        throw new IllegalArgumentException("Key is larger than the original key");
+	    }
+	    heap[bookeep[i]].key = weight;
+	    heapifyUp(bookeep[i],weight);
+	    heapifyDown(bookeep[i],weight);
 	}
+
+	private int getParentIndex(int childIndex) {
+	    return (childIndex-1) / 2;
+	}
+	
 	public int getLeftChildIndex (int parentIndex) {
 		return (parentIndex+1)*2-1;
 	}
---
+++ item.java
@@ -0,0 +1,11 @@
+public class item {
+	//Vertex
+		int V;
+		//Key value of the vertex
+		int key;
+		
+		public item (int V, int key) {
+			this.V = V;
+			this.key = key;
+		}
+}
